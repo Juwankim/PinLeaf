355a476e61ed9d4f9040c8a3e880c91d1ce6f09d
Juwan Kim <juwankim@gmail.com>
2026-08-26 10:26:20 +0900

    Initial Commit

---
+++ MySideNotes/MySideNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySideNotesApp.swift
+//  MySideNotes
+//
+//  Created by 김주완 on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MySideNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
